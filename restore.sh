b1ee7186c9144453898bb850ec7749e47a3a0b26
daedalus31 <marczykowskim@gmail.com>
2017-09-23 14:57:59 +0200

    zaczynamy

---
+++ restore.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Odtwarzam TMUX..."
+
+echo "Pobieram plugin managera."
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo 'Link symboliczny dla ~/.tmux.conf'
+ln -s ~/.tmux.conf ~/.tmux/tmux.conf
