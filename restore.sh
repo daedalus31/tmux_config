26f2b27176eceff5ae789e2df415d98e4d71627f
daedalus31 <marczykowskim@gmail.com>
2018-02-05 08:53:40 +0100

    skrypt odtwarzający, .gitignore

--- restore.sh
+++ restore.sh
@@ -6,4 +6,4 @@ echo "Pobieram plugin managera."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 echo 'Link symboliczny dla ~/.tmux.conf'
-ln -s ~/.tmux.conf ~/.tmux/tmux.conf
+ln -s ~/.tmux/tmux.conf ~/.tmux.conf
